5ef728ddc6a418f6f9ea9d6b914608f1218981fe
markcascardi <mcascardi@gmail.com>
2020-08-02 00:25:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-class BoardMember {
-  constructor(name, homeState, training) {
-    this.name = name
-    this.homeState = homeState
-    this.training = training
-  }
 
-  veto() {
-    return "No, I must disagree"
-  }
+function BoardMember(name, homeState, training) {
+  this.name = name
+  this.homeState = homeState
+  this.training = training
+}
 
-  approve() {
-    return "You can do that!"
-  }
+BoardMember.prototype.veto = function() {
+  return "No, I must disagree"
+}
 
-  doCharity() {
-    return "I like to help people."
-  }
+BoardMember.prototype.approve = function() {
+  return "You can do that!"
+}
 
-  releasePressStatement() {
-    return "You will see great things from Scuber."
-  }
+BoardMember.prototype.doCharity = function() {
+  return "I like to help people."
+}
 
-  sayHi() {
-    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-  }
+BoardMember.prototype.releasePressStatement = function() {
+  return "You will see great things from Scuber."
+}
 
+BoardMember.prototype.sayHi = function() {
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
